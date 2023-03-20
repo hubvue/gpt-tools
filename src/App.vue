85cc34ff6061d2423fe27b9d840f8dd21bffe78f
kimwangchong <kimwangchong@didiglobal.com>
2023-03-20 16:08:39 +0800

    feat: style

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,9 @@
 </template>
 
 <script setup lang="ts">
+import { useDark } from '@vueuse/core'
+
+useDark()
 </script>
 
 
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Home from '../views/Home.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      name: 'Home',
+      component: Home,
       meta: {
         title: 'GPT Tools'
       }
